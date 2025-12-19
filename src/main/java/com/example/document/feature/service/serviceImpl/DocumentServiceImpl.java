680a81c15e5dd36c413535b01da73e0609041ebf
viseth <simviseth509@gmail.com>
2025-12-19 22:15:05 +0700

    sssssss

--- src/main/java/com/example/document/feature/service/serviceImpl/DocumentServiceImpl.java
+++ src/main/java/com/example/document/feature/service/serviceImpl/DocumentServiceImpl.java
@@ -1,20 +1,28 @@
 package com.example.document.feature.service.serviceImpl;
 
 import com.example.document.app.BaseResponse;
+import com.example.document.external.dto.WorkspaceResponse;
 import com.example.document.feature.dao.DocumentDao;
 import com.example.document.feature.dto.request.DocumentRequest;
 import com.example.document.feature.dto.response.DocumentResponse;
 import com.example.document.feature.service.DocumentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @RequiredArgsConstructor
 public class DocumentServiceImpl implements DocumentService {
 
     private final DocumentDao documentDao;
+    private final WebClient workspaceClient;
     @Override
     public BaseResponse<DocumentResponse> createDocument(DocumentRequest documentRequest) {
-        return null;
+        BaseResponse<WorkspaceResponse> workspaceResponse = workspaceClient.get()
+                .uri("/{id}", documentRequest.getWorkspaceId())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<BaseResponse<WorkspaceResponse>>() {})
+                .block();
     }
 }
