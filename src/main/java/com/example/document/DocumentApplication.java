5a0f37dc2ef042fa3fb12a560bf0c11293d65260
viseth <simviseth509@gmail.com>
2025-12-26 22:28:18 +0700

    controller

--- src/main/java/com/example/document/DocumentApplication.java
+++ src/main/java/com/example/document/DocumentApplication.java
@@ -1,9 +1,7 @@
 package com.example.document;
 
-import com.example.document.external.WorkspaceServiceProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class DocumentApplication {
--- src/main/java/com/example/document/feature/controller/DocumentController.java
+++ src/main/java/com/example/document/feature/controller/DocumentController.java
@@ -1,7 +1,12 @@
 package com.example.document.feature.controller;
 
+import com.example.document.app.BaseResponse;
+import com.example.document.feature.dto.request.DocumentRequest;
+import com.example.document.feature.dto.response.DocumentResponse;
 import com.example.document.feature.service.DocumentService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,9 @@ public class DocumentController {
 
     private final DocumentService documentService;
 
-
+    @PostMapping("create")
+    public BaseResponse<DocumentResponse> createDocument(@RequestBody DocumentRequest documentRequest) {
+        BaseResponse<DocumentResponse> documentResponse = documentService.createDocument(documentRequest);
+        return documentResponse;
+    }
 }
